7e7ccb7cdb737e6df11c360c66ca6ed0074db78c
BluuesBluu <maciejczak@dotbluu.eu>
2024-08-18 20:17:25 +0200

    Rework function count

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 def count(content):
     words = content.split()
-    print(len(words))
+    return len(words)
 
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        count(file_contents)
+        num_words = count(file_contents)
+        print(f"{num_words} words found in the document")
 
 if __name__ == '__main__':
     main()
