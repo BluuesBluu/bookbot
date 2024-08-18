3c01bf3822ea848ad1584c3c3b163cc3a358c0a2
BluuesBluu <maciejczak@dotbluu.eu>
2024-08-18 20:14:56 +0200

    Add function count

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+
+def count(content):
+    words = content.split()
+    print(len(words))
+
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        count(file_contents)
+
+if __name__ == '__main__':
+    main()
